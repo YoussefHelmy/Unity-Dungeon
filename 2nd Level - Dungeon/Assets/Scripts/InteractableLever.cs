62955f9198617ff8923981e2df7654ce73eca831
AhmedElzeiny <68128852+AhmedElzeiny@users.noreply.github.com>
2020-07-17 01:39:20 +0200

    Ahmed

---
+++ 2nd Level - Dungeon/Assets/Scripts/InteractableLever.cs
@@ -0,0 +1,60 @@
+﻿
+using UnityEngine;
+
+public class InteractableLever : MonoBehaviour
+{
+    public int lightnumber;
+    public GameObject PuzzleLight;
+    public bool isInteractable = false;
+    int[] SecondPuzzleSol = new int[] {1,2,3,4,1};
+    public static int Count = 0;
+    
+    private void Start()
+    {
+         
+    }
+    public void Update()
+    { 
+       if (isInteractable && Input.GetKeyDown(KeyCode.F))
+        {
+           
+
+            
+            if(Count <= 5){
+                if (lightnumber == SecondPuzzleSol[Count])
+                { 
+                    Count++;
+                    
+                } 
+                else Count = 0;
+            }
+            Debug.Log("This is Light No." + Count);
+
+
+            if (PuzzleLight.active)
+
+            {
+                PuzzleLight.SetActive(false);
+            }
+            else
+            {
+                PuzzleLight.SetActive(true);
+            }
+        }
+
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+            isInteractable = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        isInteractable = false;
+    }
+    
+}
---
+++ 2nd Level - Dungeon/Assets/Scripts/InteractablePad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablePad : MonoBehaviour
+{
+    public int Padnumber;
+    public static int Count = 0;
+    public int[] ThirdPuzzleSol = new int[] { 1, 2, 3, 4, 1 };
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Count <= 5)
+        {
+            if (Padnumber == ThirdPuzzleSol[Count])
+            {
+                Count++;
+                Debug.Log(Count);
+            }
+            else { Count = 0;  } 
+
+        }
+    
+    }
+
+
+}
