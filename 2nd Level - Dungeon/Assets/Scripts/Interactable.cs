8ffa1c25f7788d2d6733b4fd1f94214899dfcc66
AhmedElzeiny <68128852+AhmedElzeiny@users.noreply.github.com>
2020-07-16 14:47:01 +0200

    Update Interactable.cs

--- 2nd Level - Dungeon/Assets/Scripts/Interactable.cs
+++ 2nd Level - Dungeon/Assets/Scripts/Interactable.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     public bool isInteractable = false;
-    public float radius = 3f;
+  
 
     public void Update()
     {
@@ -16,20 +16,6 @@ public class Interactable : MonoBehaviour
     }
 
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, radius);
-    }
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    isInteractable = true;
-    //}
-
-    //private void OnCollisionExit(Collision collision)
-    //{
-    //    isInteractable = false;
-    //}
     private void OnTriggerEnter(Collider other)
     {
         
@@ -50,38 +36,5 @@ public class Interactable : MonoBehaviour
 
 
 
-    //void Update()
-    //{
-    //    Checks if the player is in the collider and also if the key is pressed.
-    //    if (isInteractable && Input.GetKeyDown(KeyCode.F))
-    //    {
-    //        personalized code can go in here when activated.
-    //        Debug.Log("Interact");
-    //    }
-    //}
-
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    Compares the tag of the object entering this collider.
-    //    if (other.tag == "Player")
-    //    {
-    //        turns on interactivity
-    //        isInteractable = true;
-    //    }
-    //}
-
-
-
-
-
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    compares the tag of the object exiting this collider.
-    //    if (other.tag == "Player")
-    //    {
-    //        turns off interactivity
-    //        isInteractable = false;
-    //    }
-    //}
+   
 }
